0ec2aebc045f0e0da5340649fb32fa52b16ec9b0
5kadi <142335006+5kadi@users.noreply.github.com>
2024-11-27 20:01:39 +0300

    Update Loading.jsx

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -8,7 +8,7 @@ function Loading() {
             <div className="w-full h-screen flex justify-center items-center flex-col z-50 bg-white fixed">
                 <img
                     className="object-contain w-2/3 md:w-1/4"
-                    src="../../public/loading.gif"
+                    src="/loading.gif"
                 />
                 <h3 className="text-3xl mb-4">Loading...</h3>
             </div>
